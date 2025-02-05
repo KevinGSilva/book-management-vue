4b2eb70fca51a0b85aa7999f297524aa223a32f1
Kevin Silva <kevincaprino33@gmail.com>
2025-02-04 23:36:57 -0300

    View de listagem de autores

---
+++ src/views/Authors/AuthorsListView.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <div class="row mt-4">
+      <div class="col-lg-12">
+        <div class="card">
+          <div class="card-header">Autores</div>
+          <div class="card-body">
+            <table class="table table-bordered" id="content">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Nome</th>
+                  <th>Estado</th>
+                  <th>Opções</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td>
+                    <a href="#" class="btn btn-warning">Edit</a>
+                    <button type="button" class="btn btn-danger" data-id="">Delete</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
