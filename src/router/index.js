5378fdf90f7854305a7b3b3543025a834ad84ad7
Kevin Silva <kevincaprino33@gmail.com>
2025-02-04 21:43:15 -0300

    Registrando rota de registro

--- src/router/index.js
+++ src/router/index.js
@@ -22,6 +22,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: () => import('../views/Auth/LoginView.vue'),
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/Auth/RegisterView.vue'),
     }
   ],
 })
